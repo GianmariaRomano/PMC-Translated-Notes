880e2a371d4403c2587a8d5197de57c69d0a00a1
Gianmaria Romano <gianmaria2004@gmail.com>
2025-10-04 13:56:40 +0200

    Delete A - C Codes/Codes/strings.c

--- A - C Codes/Codes/strings.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char stringa[] = {"h", "e", "l", "l", "o"}; // First initialization.
-    char stringa[] = "hello"; // Second initialization.
-
-    char s[7] = "hello"; // Extra spaces will be kept empty.
-}
