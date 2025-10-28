6c2850345852d4d003b63d50f5f5eef40a9f8196
Gianmaria Romano <gianmaria2004@gmail.com>
2025-10-28 18:04:01 +0100

    Update matrix_vector_multiplication.c

--- 2 - MPI/Codes/matrix_vector_multiplication.c
+++ 2 - MPI/Codes/matrix_vector_multiplication.c
@@ -9,7 +9,7 @@ void mat_vec_mult(double local_A[], double local_x[], double local_y[], int loca
     x = malloc(n * sizeof(double));
     MPI_Allgather(local_x, local_n, MPI_DOUBLE, x, local_n, MPI_DOUBLE, comm); // Collect x from different processes.
 
-    for (local_i = 0; local_i < local_m, local_i++) {
+    for (local_i = 0; local_i < local_m; local_i++) {
         local_y[local_i] = 0.0;
         for (j = 0; j < n; j++) {
             local_y[local_i] += local_A[local_i * n + j] * x[j]; // For simplicity, linearize matrices as one-dimensional arrays.
