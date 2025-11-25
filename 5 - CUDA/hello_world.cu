bef8c68f8676ffaacb8992eff3ee4d7a7d58819f
Gianmaria Romano <gianmaria2004@gmail.com>
2025-11-25 18:11:07 +0100

    Update hello_world.cu

--- 5 - CUDA/hello_world.cu
+++ 5 - CUDA/hello_world.cu
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <cuda.h>
 
+// The global decorator indicates that the function must be executed on the GPU.
 __global__ void hello() {
   printf("Hello, World!");
 }
 
 int main() {
-  hello<<<1.10>>>();
+  hello<<<1.10>>>(); // Run hello() using one block containing 10 threads.
   cudaDeviceSynchronize(); // Block until the CUDA kernel terminates.
     return 1
 }
